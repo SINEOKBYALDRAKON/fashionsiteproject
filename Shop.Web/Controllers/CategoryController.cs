38b0e9c6d61145188293ad8a7d0e2d357dff5e8d
Александър Славчев <as90977457@edu.mon.bg>
2023-03-19 13:05:44 +0200

    Did shit

--- Shop.Web/Controllers/CategoryController.cs
+++ Shop.Web/Controllers/CategoryController.cs
@@ -10,138 +10,138 @@ namespace fashionsiteproject.Controllers;
 public class CategoryController : Controller
 {
     private readonly ICategory _categoryService;
-		private readonly IClothingProduct _clothingService;
-        private readonly Mapper _mapper;
+	private readonly IClothingProduct _clothingService;
+    private readonly Mapper _mapper;
 
-		public CategoryController(ICategory categoryService, IClothingProduct clothingService)
-		{
-			_categoryService = categoryService;
-			_clothingService = clothingService;
-            _mapper = new Mapper();
-		}
+	public CategoryController(ICategory categoryService, IClothingProduct clothingService)
+	{
+		_categoryService = categoryService;
+		_clothingService = clothingService;
+        _mapper = new Mapper();
+	}
 
-		public IActionResult Index()
-		{
-			var categories = _categoryService.GetAll().
-				Select(category => new CategoryListingModel
-				{
-					Name = category.Name,
-					Description = category.Description,
-					Id = category.Id,
-					ImageUrl = category.Image
-				});
-
-			var model = new CategoryIndexModel
+	public IActionResult Index()
+	{
+		var categories = _categoryService.GetAll().
+			Select(category => new CategoryListingModel
 			{
-				CategoryList = categories
-			};
-
-			return View(model);
-		}
-
-		public IActionResult Topic(int id, string searchQuery)
-		{
-			var category = _categoryService.GetById(id);
-			var clothing = _clothingService.GetFilteredClothingProducts(id, searchQuery);
-
-			var clothingListings = clothing.Select(clothing => new ClothingListingModel
-			{
-				Id = clothing.Id,
-				Name = clothing.Name,
-				InStock = clothing.InStock,
-				Price = clothing.Price,
-				ShortDescription = clothing.ShortDescription,
-				Category = _mapper.ClothingToCategoryListing(clothing),
-				ImageUrl = clothing.Image
+				Name = category.Name,
+				Description = category.Description,
+				Id = category.Id,
+				ImageUrl = category.Image
 			});
 
-			var model = new CategoryTopicModel
-			{
-				Category = _mapper.CategoryToCategoryListing(category),
-				Clothing = clothingListings
-			};
-
-			return View(model);
-		}
-
-		public IActionResult Search(int id, string searchQuery)
+		var model = new CategoryIndexModel
 		{
-			return RedirectToAction("Topic", new { id, searchQuery });
-		}
+			CategoryList = categories
+		};
 
-		[Authorize(Roles = "Admin")]
-		public IActionResult New()
+		return View(model);
+	}
+
+	public IActionResult Topic(int id, string searchQuery)
+	{
+		var category = _categoryService.GetById(id);
+		var clothing = _clothingService.GetFilteredClothingProducts(id, searchQuery);
+
+		var clothingListings = clothing.Select(clothing => new ClothingListingModel
 		{
-			ViewBag.ActionText = "create";
-			ViewBag.Action = "New";
-			ViewBag.CancelAction = "Index";
-			ViewBag.SubmitText = "Create Category";
-			return View("CreateEdit");
-		}
+			Id = clothing.Id,
+			Name = clothing.Name,
+			InStock = clothing.InStock,
+			Price = clothing.Price,
+			ShortDescription = clothing.ShortDescription,
+			Category = _mapper.ClothingToCategoryListing(clothing),
+			ImageUrl = clothing.Image
+		});
 
-		[HttpPost]
-		[Authorize(Roles = "Admin")]
-		public IActionResult New(CategoryListingModel model)
+		var model = new CategoryTopicModel
 		{
-			if (ModelState.IsValid)
-			{
-				var category = _mapper.CategoryListingToModel(model);
-				_categoryService.NewCategory(category);
-				return RedirectToAction("Topic", new { id = category.Id, searchQuery = "" });
-			}
+			Category = _mapper.CategoryToCategoryListing(category),
+			Clothing = clothingListings
+		};
 
-			ViewBag.ActionText = "create";
-			ViewBag.Action = "New";
-			ViewBag.CancelAction = "Index";
-			ViewBag.SubmitText = "Create Category";
+		return View(model);
+	}
 
-			return View("CreateEdit", model);
-		}
+	public IActionResult Search(int id, string searchQuery)
+	{
+		return RedirectToAction("Topic", new { id, searchQuery });
+	}
 
-		[Authorize(Roles = "Admin")]
-		public IActionResult Edit(int id)
+	[Authorize(Roles = "Admin")]
+	public IActionResult New()
+	{
+		ViewBag.ActionText = "create";
+		ViewBag.Action = "New";
+		ViewBag.CancelAction = "Index";
+		ViewBag.SubmitText = "Create Category";
+		return View("CreateEdit");
+	}
+
+	[HttpPost]
+	[Authorize(Roles = "Admin")]
+	public IActionResult New(CategoryListingModel model)
+	{
+		if (ModelState.IsValid)
 		{
-            ViewBag.ActionText = "change";
-            ViewBag.Action = "Edit";
-            ViewBag.CancelAction = "Topic";
-            ViewBag.SubmitText = "Save Changes";
-            ViewBag.RouteId=id;
-
-			var category = _categoryService.GetById(id);
-			if (category != null)
-			{
-				var model = _mapper.CategoryToCategoryListing(category);
-				return View("CreateEdit" ,model);
-			}
-
-			return View("CreateEdit");
+			var category = _mapper.CategoryListingToModel(model);
+			_categoryService.NewCategory(category);
+			return RedirectToAction("Topic", new { id = category.Id, searchQuery = "" });
 		}
 
-		[HttpPost]
-		[Authorize(Roles = "Admin")]
-		public IActionResult Edit(CategoryListingModel model)
+		ViewBag.ActionText = "create";
+		ViewBag.Action = "New";
+		ViewBag.CancelAction = "Index";
+		ViewBag.SubmitText = "Create Category";
+
+		return View("CreateEdit", model);
+	}
+
+	[Authorize(Roles = "Admin")]
+	public IActionResult Edit(int id)
+	{
+        ViewBag.ActionText = "change";
+        ViewBag.Action = "Edit";
+        ViewBag.CancelAction = "Topic";
+        ViewBag.SubmitText = "Save Changes";
+        ViewBag.RouteId=id;
+
+		var category = _categoryService.GetById(id);
+		if (category != null)
 		{
-			if (ModelState.IsValid)
-			{
-				var category = _mapper.CategoryListingToModel(model);
-				_categoryService.EditCategory(category);
-				return RedirectToAction("Topic", new { id = category.Id, searchQuery = "" });
-			}
-
-            ViewBag.ActionText = "change";
-            ViewBag.Action = "Edit";
-            ViewBag.CancelAction = "Topic";
-            ViewBag.SubmitText = "Save Changes";
-            ViewBag.RouteId=model.Id;
-
-			return View("CreateEdit",model);
+			var model = _mapper.CategoryToCategoryListing(category);
+			return View("CreateEdit" ,model);
 		}
 
-		[Authorize(Roles = "Admin")]
-		public IActionResult Delete(int id)
+		return View("CreateEdit");
+	}
+
+	[HttpPost]
+	[Authorize(Roles = "Admin")]
+	public IActionResult Edit(CategoryListingModel model)
+	{
+		if (ModelState.IsValid)
 		{
-			_categoryService.DeleteCategory(id);
-
-			return RedirectToAction("Index");
+			var category = _mapper.CategoryListingToModel(model);
+			_categoryService.EditCategory(category);
+			return RedirectToAction("Topic", new { id = category.Id, searchQuery = "" });
 		}
+
+        ViewBag.ActionText = "change";
+        ViewBag.Action = "Edit";
+        ViewBag.CancelAction = "Topic";
+        ViewBag.SubmitText = "Save Changes";
+        ViewBag.RouteId=model.Id;
+
+		return View("CreateEdit",model);
+	}
+
+	[Authorize(Roles = "Admin")]
+	public IActionResult Delete(int id)
+	{
+		_categoryService.DeleteCategory(id);
+
+		return RedirectToAction("Index");
+	}
 }
